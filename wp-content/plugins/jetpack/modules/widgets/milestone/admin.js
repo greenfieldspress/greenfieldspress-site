763c08f81c242c7893171b0c610938523360d738
greenfieldspress <greenfieldspress@gmail.com>
2026-03-21 12:36:26 +0000

    Updated/Added simply-static-1-1774096101

--- wp-content/plugins/jetpack/modules/widgets/milestone/admin.js
+++ wp-content/plugins/jetpack/modules/widgets/milestone/admin.js
@@ -1,3 +1,27 @@
-(function($){var $container=$('#customize-controls');if(!$container.length){$container=$('#wpbody');}
-$container.on('change','.milestone-type',function(){var $messageWrapper=$(this).parent().find('.milestone-message-wrapper');$(this).find('input[type="radio"]:checked').val()==='since'?$messageWrapper.hide():$messageWrapper.show();});function triggerChange(){$container.find('.milestone-type').trigger('change');}
-$(document).on('widget-added widget-updated',function(){triggerChange();});triggerChange();})(jQuery);
+( function ( $ ) {
+	// We could either be in wp-admin/widgets.php or the customizer.
+	var $container = $( '#customize-controls' );
+
+	if ( ! $container.length ) {
+		$container = $( '#wpbody' );
+	}
+
+	$container.on( 'change', '.milestone-type', function () {
+		var $messageWrapper = $( this ).parent().find( '.milestone-message-wrapper' );
+
+		$( this ).find( 'input[type="radio"]:checked' ).val() === 'since'
+			? $messageWrapper.hide()
+			: $messageWrapper.show();
+	} );
+
+	function triggerChange() {
+		$container.find( '.milestone-type' ).trigger( 'change' );
+	}
+
+	// Used when adding widget via customizer or saving settings.
+	$( document ).on( 'widget-added widget-updated', function () {
+		triggerChange();
+	} );
+
+	triggerChange();
+} )( jQuery );
