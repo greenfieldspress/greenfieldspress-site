b5ab34bd0f44af434e4dc5dea1c6849e0804932d
greenfieldspress <greenfieldspress@gmail.com>
2026-03-21 12:36:12 +0000

    Updated/Added simply-static-1-1774096101

--- wp-content/plugins/jetpack/modules/theme-tools/js/suggest.js
+++ wp-content/plugins/jetpack/modules/theme-tools/js/suggest.js
@@ -1,1 +1,7 @@
-jQuery(function($){$('#customize-control-featured-content-tag-name input').suggest(ajaxurl+'?action=ajax-tag-search&tax=post_tag',{delay:500,minchars:2});});
+/* global ajaxurl:true */
+jQuery( function ( $ ) {
+	$( '#customize-control-featured-content-tag-name input' ).suggest(
+		ajaxurl + '?action=ajax-tag-search&tax=post_tag',
+		{ delay: 500, minchars: 2 }
+	);
+} );
