1af6f4d5d6aa0a1c9de3f86d58f59cb68c7d1c17
greenfieldspress <greenfieldspress@gmail.com>
2026-03-21 09:46:09 +0000

    Updated/Added simply-static-1-1774084420

---
+++ wp-content/themes/kadence/inc/dashboard/react/src/recomended.js
@@ -0,0 +1,1 @@
+import{__}from'@wordpress/i18n';const{Fragment}=wp.element;export const RecommendedTab=()=>{return(<Fragment><p>{__('This area is for Recommended Plugins.','kadence')}</p></Fragment>);};export default RecommendedTab;
