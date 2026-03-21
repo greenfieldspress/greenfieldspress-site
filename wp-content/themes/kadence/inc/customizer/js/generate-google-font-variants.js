a731edd237a3008b37bcfc18bb0f685d4847755d
greenfieldspress <greenfieldspress@gmail.com>
2026-03-21 09:46:00 +0000

    Updated/Added simply-static-1-1774084420

---
+++ wp-content/themes/kadence/inc/customizer/js/generate-google-font-variants.js
@@ -0,0 +1,4 @@
+$.getJSON('https://www.googleapis.com/webfonts/v1/webfonts?key={{API_KEY}}',function(data){$.each(data.items,function(index,font){var category=[];category.push(font.category);var variants=font.variants;var weights=font.variants.slice();var styles=['normal'];for(var i=0;i<weights.length;i++){if(weights[i].includes('italic')){weights.splice(i,1);}};if(variants.includes('italic')){styles.push('italic');}
+$('.google-fonts').append("'"+font.family+"' => array( 'v' => array(");for(var i=0;i<variants.length;i++){if(0===i){$('.google-fonts').append("'"+variants[i]+"'");}else{$('.google-fonts').append(",'"+variants[i]+"'");}}
+$('.google-fonts').append(")"+",'c' => array(");for(var i=0;i<category.length;i++){if(0===i){$('.google-fonts').append("'"+category[i]+"'");}else{$('.google-fonts').append(",'"+category[i]+"'");}}
+$('.google-fonts').append(")"+"),");});});
