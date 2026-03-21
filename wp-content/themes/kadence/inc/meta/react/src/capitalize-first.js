5c34b01646c0a642917b64dd75961717b9c17428
greenfieldspress <greenfieldspress@gmail.com>
2026-03-21 12:58:34 +0000

    Updated/Added simply-static-1-1774096101

--- wp-content/themes/kadence/inc/meta/react/src/capitalize-first.js
+++ wp-content/themes/kadence/inc/meta/react/src/capitalize-first.js
@@ -1,1 +1,8 @@
-export default function capitalizeFirstLetter(string){return string.charAt(0).toUpperCase()+string.slice(1);}
+/**
+ * function to return string with capital letter.
+ * @param {string} string the word string.
+ * @returns {string} with capital letter.
+ */
+export default function capitalizeFirstLetter( string ) {
+	return string.charAt( 0 ).toUpperCase() + string.slice( 1 );
+}
