9bbae3e55fb25b046e4269b9f6a48811c59e2d45
greenfieldspress <greenfieldspress@gmail.com>
2026-03-21 12:50:59 +0000

    Updated/Added simply-static-1-1774096101

--- wp-content/plugins/matomo/app/plugins/Overlay/client/translations.js
+++ wp-content/plugins/matomo/app/plugins/Overlay/client/translations.js
@@ -1,2 +1,30 @@
-var Piwik_Overlay_Translations=(function(){var translations=[];return{initialize:function(callback){Matomo_Overlay_Client.api('Overlay.getTranslations',function(data){translations=data;callback();});},get:function(identifier){if(typeof translations[identifier]=='undefined'){return identifier;}
-return translations[identifier];}};})();
+var Piwik_Overlay_Translations = (function () {
+
+    /** Translations strings */
+    var translations = [];
+
+    return {
+
+        /**
+         * Initialize translations module.
+         * Callback is triggered when data is available.
+         */
+        initialize: function (callback) {
+            // Load translation data
+            Matomo_Overlay_Client.api('Overlay.getTranslations', function (data) {
+                translations = data;
+                callback();
+            });
+        },
+
+        /** Get translation string */
+        get: function (identifier) {
+            if (typeof translations[identifier] == 'undefined') {
+                return identifier;
+            }
+            return translations[identifier];
+        }
+
+    };
+
+})();
