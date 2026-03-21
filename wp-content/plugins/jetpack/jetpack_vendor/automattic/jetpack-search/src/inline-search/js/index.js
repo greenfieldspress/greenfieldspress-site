9dbb8e2795ce07e52fb4b90ace712bfccfe1f1c5
greenfieldspress <greenfieldspress@gmail.com>
2026-03-21 12:33:31 +0000

    Updated/Added simply-static-1-1774096101

--- wp-content/plugins/jetpack/jetpack_vendor/automattic/jetpack-search/src/inline-search/js/index.js
+++ wp-content/plugins/jetpack/jetpack_vendor/automattic/jetpack-search/src/inline-search/js/index.js
@@ -1,1 +1,6 @@
-import'../styles/corrected-query.scss';import'./corrected-query';
+/**
+ * Entry point for inline search styles.
+ * This file imports the CSS and the modules for inline search.
+ */
+import '../styles/corrected-query.scss';
+import './corrected-query';
