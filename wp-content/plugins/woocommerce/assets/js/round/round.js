9d232cbbfc4518ac40b666c158e46d9a7d2e00f2
greenfieldspress <greenfieldspress@gmail.com>
2026-03-21 09:45:13 +0000

    Updated/Added simply-static-1-1774084420

---
+++ wp-content/plugins/woocommerce/assets/js/round/round.js
@@ -0,0 +1,2 @@
+function round(value,precision,mode){var m,f,isHalf,sgn;precision|=0;m=Math.pow(10,precision);value*=m;sgn=(value>0)|-(value<0);isHalf=value%1===0.5*sgn;f=Math.floor(value);if(isHalf){switch(mode){case'2':case'PHP_ROUND_HALF_DOWN':value=f+(sgn<0);break;case'3':case'PHP_ROUND_HALF_EVEN':value=f+(f%2*sgn);break;case'4':case'PHP_ROUND_HALF_ODD':value=f+!(f%2);break;default:value=f+(sgn>0);}}
+return(isHalf?value:Math.round(value))/ m;}
